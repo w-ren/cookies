63f319d9b24ebaf27dd72b2a717eb7cc899f9257
w-ren <irene.weixin@gmail.com>
2020-12-04 17:54:13 -0800

    Reorganized notes

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const app = new Vue({
   el: '#app',
   data: {
-    notes: ['You got this!','Dont worry about it you can do it','This will soon pass...just like your grades on your finals', 'you are capable of many things','😋',],
+    notes: [
+      'You got this!',
+      'Dont worry about it you can do it',
+      'This will soon pass...just like your grades on your finals',
+      'you are capable of many things',
+      '😋',
+      ],
   },
   methods: {
     chooseNote() {
