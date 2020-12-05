a8cc3d086374f5778920a3c09176fb8ed23270bb
w-ren <irene.weixin@gmail.com>
2020-12-04 17:48:11 -0800

    transfer from codepen

---
+++ script.js
@@ -0,0 +1,12 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    notes: ['You got this!','Dont worry about it you can do it','This will soon pass...just like your grades on your finals', 'you are capable of many things','😋',],
+  },
+  methods: {
+    chooseNote() {
+      const randomNumber = Math.floor(Math.random() * this.notes.length);
+      return this.notes[randomNumber];
+    }
+  }, 
+});
