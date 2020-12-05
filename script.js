5194effac796305dcfbdc56470af8520658a4931
w-ren <irene.weixin@gmail.com>
2020-12-04 21:00:12 -0800

    totally normal stuff

--- script.js
+++ script.js
@@ -7,12 +7,21 @@ const app = new Vue({
       'This will soon pass...just like your grades on your finals',
       'you are capable of many things',
       '😋',
-      ],
+    ],
+    biscuitFlag: 'https://cdn.pixabay.com/photo/2012/04/10/16/14/union-jack-26119__340.png',
+    biscuitMessage: 'Long live the Queen!',
+    biscuitTime: false,
   },
   methods: {
     chooseNote() {
       const randomNumber = Math.floor(Math.random() * this.notes.length);
       return this.notes[randomNumber];
     }
-  }, 
+  },
+  mounted() {
+    const chance = Math.random();
+    if (chance < 0.005) {
+      this.biscuitTime = true;
+    }  
+  },
 });
